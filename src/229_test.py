a3505ed74caebd50cc03225c5461cab37ce2f9cb
OkaNim <OKA.Hiroyuki@nims.go.jp>
2024-05-02 14:35:10 +0900

    Add files via upload

---
+++ src/229_test.py
@@ -0,0 +1,20 @@
+import src_039_recognize_polymer_name_240426 as src_039
+import src_229_recognize_polymer_refsign_240426 as src_229
+
+
+toks = ["in", "polystyrene-block-poly", '(', "ethylene", "oxide", ')', '(', "PS-b-PEO", ')', "bottlebrush", "block", "copolymers", '(', "BBCP", ')', "upon", "The", "BBCPs", "are", "soluble", "in", "organic", "solvents", "."]
+
+pos_tags = ["IN", "NN", "-LRB-", "NN", "NN", "-RRB-", "-LRB-", "NN", "-RRB-", "NN", "NN", "NNS", "-LRB-", "NN", "-RRB-", "IN", "DT", "NNS", "VBP", "JJ", "IN", "JJ", "NNS", "."]
+# It is necessary to recognize pos_tags in advance using Stanford Core NLP.
+
+
+refsign_tags, refsign_abbs = src_229.main(toks, pos_tags)
+
+polymer_tags = src_039.main(toks)
+
+
+print("\nrefsign_tags\n", refsign_tags)
+print("\npolymer_tags\n", polymer_tags)
+print("")
+print("\nrefsign_abbs\n", refsign_abbs)
+print("\n\n")
---
+++ src/src_229_recognize_polymer_refsign_240426.py
@@ -0,0 +1,255 @@
+# import
+import os, re
+
+
+
+
+# main
+def main(toks, tags_pos):
+    refsign_words = input_polymer_refsign_words()                              # f
+
+
+    tags = ['O'] * len(toks)
+    words = refsign_words
+    tags, abbs = search_refsign(toks, tags, tags_pos, words, abb=0)            # f
+
+    words = list(abbs.keys())
+    tags = search_refsign(toks, tags, tags_pos, words, abb=1)[0]               # f
+
+
+    return tags, abbs
+
+
+
+
+# f
+def input_polymer_refsign_words():
+    indir = "./229_file"
+    infname = "polymer-refsign-words.txt"
+    refsign_words = []
+    with open(indir + '/' + infname, 'r', encoding = "utf-8") as f:
+        for x in f:
+            x = x[:-1]
+            refsign_words.append(x)
+
+    return refsign_words
+
+
+
+def search_refsign(TOK, TAG, POS, REFSIGN_TERM, abb):
+    ABB = {}
+    for i, tok in enumerate(TOK):
+        if TAG[i] != "O": continue
+        if abb == 0: tok = tok.lower()
+
+        for term in REFSIGN_TERM:
+            if term in tok:
+
+                start, end = check_span(i, POS, TOK)                           # f
+                end_2 = -1
+
+                if abb == 0:
+                    cnt_bracket = check_cnt_bracket(start, end, POS)           # f
+                    if cnt_bracket % 2 == 1: continue
+
+                # if TOK[start].lower() in ["a", "an"]: continue
+                if POS[end][:2] in ["VB"]: continue
+                tok_end = TOK[end]
+                if abb == 0: tok_end = TOK[end].lower()
+                if not term in tok_end:
+                    if POS[end][:2] == "-R" and POS[i + 1][:2] == "-L":
+                        if end - (i + 1) == 2:
+                            if TOK[end - 1].isnumeric(): end = i
+                            else:
+                                if abb == 0:
+                                    refsign_abb = check_abbreviation(TOK[end - 1])    # f
+                                    if refsign_abb != "":
+                                        REFSIGN = TOK[start : i + 1]
+                                        refsign_long = longform_for_abb(REFSIGN, refsign_abb)    # f
+                                        if not refsign_abb in ABB:
+                                            ABB[refsign_abb] = refsign_long
+                                        end_2 = end - 1
+                        end = i
+                    else: continue
+
+                TAG = tag_BIES(TAG, start, end, end_2)                         # f
+
+
+    return TAG, ABB
+
+
+
+def check_span(i, POS, TOK):
+    OK_POS_1 = ["NN", "JJ", "RB", "CD", "HY", "-L", "-R", "SY", "AD", "PD"]
+    OK_POS_2 = OK_POS_1
+    # OK_POS_2 = ["NN", "CD", "HY", "SY"]
+
+
+    start, end = 0, len(POS) - 1
+    for j in range(i)[::-1]:
+        if not POS[j][:2] in OK_POS_1:
+            if POS[j] == "DT":
+                start = j
+                break
+            if POS[j] == "VBN": continue
+            if POS[j][:2] == "CC":
+                if TOK[j].lower() == "both": continue
+                if 0 < j < len(POS) - 1 and POS[j - 1][:2] == POS[j + 1][:2]: continue
+            start = j + 1
+            break
+    for j in range(i + 1, len(POS)):
+        if not POS[j][:2] in OK_POS_2:
+            if POS[j][:2] == "CC":
+                if 0 < j < len(POS) - 1 and POS[j - 1][:2] == POS[j + 1][:2]: continue
+            end = j - 1
+            break
+
+
+    return start, end
+
+
+
+def check_cnt_bracket(start, end, POS):
+    cnt_bracket = 0
+    for j in range(start, end + 1):
+        if "-L" in POS[j] or "-R" in POS[j]: cnt_bracket += 1
+
+    return cnt_bracket
+
+
+
+def check_abbreviation(tok):
+    refsign_abb = ""
+    M = re.findall(r"[A-Z]", tok)
+    if len(M) / len(tok) >= 0.5:
+        if not tok.isnumeric(): refsign_abb = tok
+
+    return refsign_abb
+
+
+
+def longform_for_abb(REFSIGN, refsign_abb):
+    if refsign_abb[-1] == "s": refsign_abb = refsign_abb[:-1]
+
+    long = "_"
+    num = len(refsign_abb)
+    while num > len(REFSIGN): num -= 1
+    if num <= len(REFSIGN):
+        cnt_OK = 0
+        if refsign_abb[0].upper() == REFSIGN[-num:][0][0].upper(): cnt_OK = 1
+        else:
+            if num > 1:
+                num -= 1
+                if refsign_abb[0].upper() == REFSIGN[-num:][0][0].upper(): cnt_OK = 1
+        if cnt_OK == 1:
+            abb = ""
+            i2 = -1
+            for i, char_abb in enumerate(refsign_abb):
+                i2 += 1
+                if i2 < num:
+                    word = REFSIGN[-num:][i2]
+                    if word[0].upper() == char_abb.upper(): abb += word[0].upper()
+                    else:
+                        char = check_middle_char(REFSIGN[-num:][i2 - 1], char_abb)    # f
+                        if char != "":
+                            abb += char
+                            i2 -= 1
+                elif i2 == num:
+                    char = check_middle_char(REFSIGN[-num:][i2 - 1], char_abb)    # f
+                    if char != "": abb += char
+            if abb == refsign_abb.upper(): long = " ".join(REFSIGN[-num:])
+
+
+    return long
+
+
+
+def check_middle_char(word, abb_char):
+    char = ""
+    for i, x in enumerate(word):
+        if i == 0: continue
+        if x.upper() == abb_char.upper():
+            char = x.upper()
+            break
+
+    return char
+
+
+
+def tag_BIES(TAG, start, end, end_2, label=None):
+    if start == end: TAG[start] = "S"
+    else:
+        for j in range(start, end + 1):
+            if j == start: TAG[j] = "B"
+            elif j == end: TAG[j] = "E"
+            else: TAG[j] = "I"
+    if end_2 != -1: TAG[end_2] = "S"
+
+
+    if label is not None:
+        for tag in TAG:
+            if tag != 'O': tag += '-' + label
+
+
+    return TAG
+
+
+
+def extract_refsign(src_001, TOK, TAG, PTAG=None):
+    REFSIGN = []
+    for i, x in enumerate(TAG):
+        refsign = ""
+        j2 = -1
+        if "S" in x : j2 = i
+        elif "B" in x:
+            for j in range(i + 1, len(TAG)):
+                if "E" in TAG[j]:
+                    j2 = j
+                    break
+        if j2 != -1:
+            refsign = " ".join(TOK[i : j2 + 1])
+            REFSIGN.append([refsign, "_", "_"])
+
+            if PTAG != None:
+                POLYMER = []
+                for j in range(i, j2 + 1):
+                    k2 = -1
+                    if PTAG[j][0] == "S": k2 = j
+                    elif PTAG[j][0] == "B":
+                        k2 = src_001.iter_end(PTAG, start=j, end_word="E")                         # import
+                    if k2 != -1:
+                        polymer = " ".join(TOK[j : k2 + 1])
+                        POLYMER.append(polymer)
+                        if k2 < len(TOK) - 1 and TOK[k2 + 1] == ")": k2 += 1
+                        attached = " ".join(TOK[k2 + 1 : j2 + 1])
+                if POLYMER != []:
+                    REFSIGN[-1][1] = "; ".join(POLYMER)
+                    REFSIGN[-1][2] = attached
+
+
+    return REFSIGN
+
+
+
+def modify_polymer_tag(TOK, TAG, REFSIGN_ABB):
+    for i, tag in enumerate(TAG):
+        if tag[0] == "S":
+            for abb in REFSIGN_ABB:
+                if abb in TOK[i]:
+                    TAG[i] = "O"
+                    break
+        if tag[0] == "E":
+            for abb in REFSIGN_ABB:
+                if abb in TOK[i]:
+                    TAG[i] = "O"
+                    TAG[i - 1] = "E"
+                    break
+
+    for i, tag in enumerate(TAG):
+        if tag[0] != "O":
+            if len(tag) == 1: TAG[i] += "-polymer"
+
+
+    return TAG
+
+
